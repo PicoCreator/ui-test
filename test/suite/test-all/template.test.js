b813c6c17d83d02ff7bffe94c8e3151d60829542
Eugene Cheah <eugeneqin@gmail.com>
2018-04-10 12:13:34 +0800

    Setup overall to only check auth (for now)

--- test/suite/test-all/template.test.js
+++ test/suite/test-all/template.test.js
@@ -6,13 +6,11 @@
 //
 ////////////////////////////////////////////////
 
-/*
 // Authentication 
 //--------------------------------------
 TEST.run("case/auth/invalid-login");
 TEST.run("case/auth/valid-login");
-*/
 
 // Case management
 //--------------------------------------
-TEST.run("case/case-management/create-a-new-case-window (multiuser)");
+// TEST.run("case/case-management/create-a-new-case-window (multiuser)");
