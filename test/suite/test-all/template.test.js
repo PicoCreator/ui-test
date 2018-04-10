3d54366f685137629e4d5d32de8ddb363290742c
Eugene Cheah <eugeneqin@gmail.com>
2018-04-10 17:09:38 +0800

    tweaks on test-all template

--- test/suite/test-all/template.test.js
+++ test/suite/test-all/template.test.js
@@ -13,4 +13,4 @@ TEST.run("case/auth/valid-login");
 
 // Case management
 //--------------------------------------
-// TEST.run("case/case-management/create-a-new-case-window (multiuser)");
+TEST.run("case/case-management/create-a-new-case-window (multiple-users-repeated)");
