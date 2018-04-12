c12ae46b1713aa88869ad8932d587ee683a058e0
Eugene Cheah <eugeneqin@gmail.com>
2018-04-12 17:16:15 +0800

    Invalid email test case

---
+++ test/case/auth/invalid-signup (invalid-email).test.js
@@ -0,0 +1,27 @@
+//////////////////////////////////////////////////////////
+//
+//  test case : invalid-signup (invalid-email)
+//
+//  Ensure email form is validated in signup
+//
+//////////////////////////////////////////////////////////
+
+// Include the data-config
+TEST.run("data-config");
+
+// Go to the signup page
+I.goTo( DATA.site );
+
+// Click on signup
+I.click( "Sign Up" );
+
+// Fill up the form
+I.fill("Email", "not-a-valid-email-format" );
+I.fill("Password", "not-a-valid-email-format" );
+
+// See error message
+I.see("Email address is invalid");
+
+// Click on submit (expect nothing to happen)
+I.click("SUBMIT");
+I.see("Email address is invalid");
