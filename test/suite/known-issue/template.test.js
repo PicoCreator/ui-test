de51dc01ecbece5008a3f385f967f8a6c8af2f6f
Eugene Cheah <eugeneqin@gmail.com>
2018-08-31 11:55:15 +0800

    WIP testupdate

--- test/suite/known-issue/template.test.js
+++ test/suite/known-issue/template.test.js
@@ -1,3 +1,8 @@
 // Invalid Signup
 //--------------------------------------
+
+// Invalid email test case
 TEST.run("case/auth/invalid-signup (invalid-email)");
+
+// Image upload check
+TEST.run("case/case-management/image-upload-check");
