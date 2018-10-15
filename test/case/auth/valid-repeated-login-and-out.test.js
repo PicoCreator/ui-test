4fb2beadf37fb393f7e4e53dac102e467349bba8
Eugene Cheah <eugeneqin@gmail.com>
2018-10-15 15:03:05 +0800

    WIP test cases refactoring

--- test/case/auth/valid-repeated-login-and-out.test.js
+++ test/case/auth/valid-repeated-login-and-out.test.js
@@ -11,7 +11,7 @@ TEST.run("case/auth/valid-login");
 // Click the home navigator, and perform a logout
 for(let i=0; i<100; i++) {
 	I.click('//*[@id="app"]/div/div/div[1]/div[1]/div[1]/div/button');
-	//I.see("Logout");
+	I.see("Logout");
 	I.click("Logout");
 	I.see("Please login to continue");
 }
