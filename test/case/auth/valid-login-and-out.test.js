6d87179cda196f05c1152d13e01e07c822f4c901
Eugene Cheah <eugeneqin@gmail.com>
2019-01-20 19:41:08 +0800

    backward compatibility testing

--- test/case/auth/valid-login-and-out.test.js
+++ test/case/auth/valid-login-and-out.test.js
@@ -9,7 +9,12 @@ TEST.run("data-config");
 TEST.run("case/auth/valid-login");
 
 // Click the home navigator, and perform a logout
-I.click('//html/body', 25, 25);
+if( I.see$('//*[@id="app"]/div/div/div[1]/div[1]/div[1]/div/button') ) {
+	I.click('//*[@id="app"]/div/div/div[1]/div[1]/div[1]/div/button', 25, 25);
+} else {
+	I.click('//html/body', 25, 25);
+}
+
 I.see("Logout");
 I.click("Logout");
 I.see("Please login to continue");
